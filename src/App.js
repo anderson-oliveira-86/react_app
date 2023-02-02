78fef8a192e55285934c1880ccb1cdb1e542356f
Anderson de Oliveira <anderson.java.dev@gmail.com>
2023-02-02 07:08:43 -0300

    Utilizando React fragments

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
         idade="36" 
         profissao="Dev" 
         foto="https://via.placeholder.com/150"/>
+        <List/>
     </div>
   );
 }
