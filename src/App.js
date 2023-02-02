d504f91fa3cc2748bec9dc7f1cb0fc720ff13cf9
Anderson de Oliveira <anderson.java.dev@gmail.com>
2023-02-01 21:57:42 -0300

    Trabalhando com props (Propriedades)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
 
-  const name = 'Matheus'
-  const newName = name.toUpperCase()
-
-  function sum(a,b){
-    return a + b
-  }
-
-  const url = "https://via.placeholder.com/150"
+  const nome = "Maria"
 
   return (
     <div className="App">
-      <p>Olá, {newName}</p>
-      <p>Divisão {50 / 2}</p>
-      <p>Soma: {sum(2,3)}</p>
-      <img src={url} alt='Minha Imagem'/>
       <HelloWorld/>
+      <SayMyName nome="Andersoooon"/>
+      <SayMyName nome="Muleke"/>
+      <SayMyName nome={nome}/>
+      <Pessoa 
+        nome="Anderson" 
+        idade="36" 
+        profissao="Dev" 
+        foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
